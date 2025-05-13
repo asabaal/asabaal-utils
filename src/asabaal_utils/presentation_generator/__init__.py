96203cb31ae7743f6fedc0cafc4849569c1f36b0
Asabaal Horan <156483330+asabaal@users.noreply.github.com>
2025-05-13 14:19:38 -0500

    Add presentation generator module

---
+++ src/asabaal_utils/presentation_generator/__init__.py
@@ -0,0 +1,7 @@
+"""
+Presentation Generator module for Asabaal Utilities.
+
+This module provides tools for generating interactive HTML presentations from JSON data.
+"""
+
+from .presentation_html import generate_presentation_html
